ae99a417d2fbea8f4c5545b8a62c6a7f7730299b
tej00001 <tejachintu207@gmail.com>
2023-03-27 23:59:28 +0530

    Adding Task1 Signup Screen

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Button } from 'react-bootstrap';
+import './App.css';
+import SignupDetails from './components/signup';
+
+function App() {
+  return (
+    <div className="App">
+     
+     <SignupDetails />
+    </div>
+  );
+};
+
+export default App;
