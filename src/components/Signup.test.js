40274b625ec931b2d5e80f8ef54bc906397ffd75
tej00001 <tejachintu207@gmail.com>
2023-03-30 11:54:00 +0530

    Adding Task2 TDD

---
+++ src/components/Signup.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Signup from './Signup';
+
+describe('Signup component', () => {
+  test('renders email, password, and confirm password input fields', () => {
+    render(<Signup />);
+    const emailInput = screen.getByPlaceholderText('Enter email');
+    const passwordInput = screen.getByPlaceholderText('Password');
+    const confirmPasswordInput = screen.getByPlaceholderText('Confirm Password');
+    expect(emailInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+    expect(confirmPasswordInput).toBeInTheDocument();
+  });
+});
