584b798fbc0f87d3190cda197ef34358d917c90d
tej00001 <tejachintu207@gmail.com>
2023-04-01 09:17:42 +0530

    Adding Task3 Login Screen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import { Button } from 'react-bootstrap';
 import './App.css';
 import SignupDetails from './components/signup';
+import { Switch,Route } from 'react-router-dom';
+import LoginDetails from './components/login';
 
 function App() {
   return (
     <div className="App">
+      
      
+      <Switch>
+        <Route exact path="/login">
+             <LoginDetails /> 
+          </Route>
+      <Route exact path="/sigup">
      <SignupDetails />
+     </Route>
+     </Switch>
     </div>
   );
 };
